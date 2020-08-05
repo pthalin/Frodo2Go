a1c6f59e09b9ab35d1597ae58598a5b32737c8f2
Patrik Thalin <patrik@thalin.se>
2020-08-05 15:07:17 +0200

    Fix icon adn reslese script

---
+++ release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+rm -rf release
+mkdir release
+mkdir release/Frodo2Go
+mkdir release/menu
+
+cd src
+#make clean&&make -f Makefile.bittboy 
+cd ..
+
+cp src/Frodo release/Frodo2Go/
+cp src/run.sh release/Frodo2Go/
+
+cp controls.man.txt release
+
+cp c64_icon.png release
+
+cd release
+zip Frodo2Go.zip -r Frodo2Go/ -r menu/ controls.man.txt c64_icon.png 
+cd ..
+cp release/Frodo2Go.zip .
+
+
+
+
+
